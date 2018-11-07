122e387972907b868cf0fa3b89921808d17235e8
carpemc <carpemc@gmail.com>
2018-11-06 23:47:30 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Butter Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fat Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crown Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Deluxe Cheeseburger Burger', 1);
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 8080;
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgersController.js");
+
+//set handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});
